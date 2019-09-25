4d0f09b61cf11e13aeb4a28c2911b7ce4c03ce05
Kamran Ali <kgali21@gmail.com>
2019-09-25 13:47:20 -0700

    server start

--- server.js
+++ server.js
@@ -1,53 +1,10 @@
 require('dotenv').config();
-require('./lib/models/connect')();
-const express = require('express');
-const app = express();
-const Queen = require('./lib/models/queen');
+const app = require('./lib/app');
 
-app.use(express.json());
+const { createServer } = require('http');
+const server = createServer(app);
 
-app.get('/api/queen', (req, res, next) => {
-  Queen.find()
-    .then(queen => {
-      res.json(queen);
-    })
-    .catch(next);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
 });
-
-app.get('/api/queen/:id', (req, res, next) => {
-  Queen.findById(req.params.id)
-    .then(queen => {
-      res.json(queen);
-    })
-    .catch(next);
-});
-
-app.post('/api/queen', (req, res, next) => {
-  Queen.create(req.body)
-    .then(queen => {
-      res.json(queen);
-    })
-    .catch(next);
-});
-
-app.put('/api/queen/:id', (req, res, next) => {
-  Queen.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true }
-  )
-    .then(queen => {
-      res.json(queen);
-    })
-    .catch(next);
-});
-
-app.delete('/api/queen/:id', (req, res, next) => {
-  Queen.findByIdAndRemove(req.params.id)
-    .then(removed => {
-      res.json(removed);
-    })
-    .catch(next);
-});
-
-app.listen(3000, () => console.log('server running on 3000'));
